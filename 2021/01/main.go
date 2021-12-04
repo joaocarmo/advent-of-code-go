b05d04912574e27ef28569b3c1e81374a7ab89ae
João Carmo <kingjoao@gmail.com>
2021-12-04 17:50:57 +0000

    Adds the solution for the puzzle 1, part 2

--- 2021/01/main.go
+++ 2021/01/main.go
@@ -58,8 +58,19 @@ func getStringFromResult(result int) string {
 	return "no change"
 }
 
+// sumArrayElements sums the elements of an array.
+func sumArrayElements(array []int) int {
+	sum := 0
+
+	for _, each := range array {
+		sum += each
+	}
+
+	return sum
+}
+
 // getFinalAnswer returns the total number of times the result increased.
-func getFinalAnswer(txtlines []string) int {
+func getFinalAnswerPartOne(txtlines []string) int {
 	var answer int
 	var prev int
 	var result int
@@ -93,6 +104,59 @@ func getFinalAnswer(txtlines []string) int {
 	return answer
 }
 
+// getFinalAnswerPartTwo returns the total number of times the sum of three
+// consecutive measures increased.
+func getFinalAnswerPartTwo(txtlines []string) int {
+	var answer int
+	var prev int
+	var accum []int
+	var sum int
+	var result int
+	var resultStr string
+
+	for _, eachline := range txtlines {
+		// we'll convert each line from a string to an integer
+		num, err := strconv.Atoi(eachline)
+
+		if err != nil {
+			log.Fatalf("failed converting string to integer: %s", err)
+		}
+
+		// we need to accumulate the previous three measurements
+		if len(accum) < 3 {
+			accum = append(accum, num)
+		} else {
+			// we need to remove the first measurement and add the new one
+			accum = append(accum[1:], num)
+		}
+
+		// if we have three measurements, we can calculate the sum
+		if len(accum) == 3 {
+			sum = sumArrayElements(accum)
+		} else {
+			continue
+		}
+
+		// compare the previous result to the current result
+		if prev != 0 {
+			result = comparePrevToCurrent(prev, sum)
+			resultStr = getStringFromResult(result)
+
+			if result == 1 {
+				answer++
+			}
+		} else {
+			resultStr = "N/A - no previous sum"
+		}
+
+		prev = sum
+
+		fmt.Printf("%d\t(%s)\n", sum, resultStr)
+	}
+
+	return answer
+}
+
 // main is the entry point for the application.
 func main() {
 	// get the filename from the command line
@@ -106,8 +170,12 @@ func main() {
 	filename := args[0]
 	txtlines := readFile(filename)
 
-	// get the final answer
-	answer := getFinalAnswer(txtlines)
+	// get the final answers
+	answerPartOne := getFinalAnswerPartOne(txtlines)
 
-	fmt.Printf("The answer is: %d\n", answer)
+	fmt.Printf("[Part One] The answer is: %d\n", answerPartOne)
+
+	answerPartTwo := getFinalAnswerPartTwo(txtlines)
+
+	fmt.Printf("[Part Two] The answer is: %d\n", answerPartTwo)
 }
