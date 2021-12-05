132c2f99c299faf2b13e556e851389868978f7bf
João Carmo <kingjoao@gmail.com>
2021-12-05 12:27:18 +0000

    Adds the day 4, part 1 problem

--- 2021/create-new-from-template.sh
+++ 2021/create-new-from-template.sh
@@ -5,6 +5,7 @@ set -e
 NEW_PROJECT_NAME="$1"
 WORKDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 TEMPLATEDIR="$WORKDIR/template"
+NEW_PROJECT_DIR="$WORKDIR/$NEW_PROJECT_NAME"
 
 # stop if no project name is given
 if [ -z "$NEW_PROJECT_NAME" ]; then
@@ -13,7 +14,7 @@ if [ -z "$NEW_PROJECT_NAME" ]; then
 fi
 
 # stop if project already exists
-if [ -d "$WORKDIR/$NEW_PROJECT_NAME" ]; then
+if [ -d "$NEW_PROJECT_DIR" ]; then
   >&2 echo "ERROR: Project already exists"
   exit 1
 fi
@@ -21,8 +22,6 @@ fi
 echo "Creating new project '$NEW_PROJECT_NAME' from template..."
 
 # copy the template to the new project
-NEW_PROJECT_DIR="$WORKDIR/$NEW_PROJECT_NAME"
-
 cp -r "$TEMPLATEDIR" "$NEW_PROJECT_DIR"
 
 # replace the project name in the template
