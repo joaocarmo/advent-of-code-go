1439d60624cd5739b5e0acbb95fdafeaa7a7bdb1
João Carmo <kingjoao@gmail.com>
2021-12-19 00:18:22 +0000

    Adds solution for day 8, part 1

---
+++ 2021/08/main.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/joaocarmo/advent-of-code/helpers"
+)
+
+const verbose = true
+
+var numOfSignalsToDigit = map[int]int{
+	0: 6,
+	1: 2,
+	2: 5,
+	3: 5,
+	4: 4,
+	5: 5,
+	6: 6,
+	7: 3,
+	8: 7,
+	9: 6,
+}
+
+// printOutputMap prints the output map.
+func printOutputMap(output map[int]map[string][]int) {
+	if verbose {
+		line := 0
+
+		// loop through the lines
+		for _, lineDigits := range output {
+			line += 1
+
+			// loop through the output
+			for signal, digits := range lineDigits {
+				fmt.Printf("Line %-2d | %-8s: %v\n", line, signal, digits)
+			}
+		}
+	}
+}
+
+// parseInput parses the input into a map of signal patterns and output values.
+func parseInput(lines []string) ([][]string, [][]string) {
+	allSignals := make([][]string, len(lines))
+	allOutput := make([][]string, len(lines))
+
+	for i, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		// split the line into signal and output
+		result := strings.Split(line, "|")
+		signalPatterns := strings.TrimSpace(result[0])
+		outputValues := strings.TrimSpace(result[1])
+
+		// split the signal patterns into an array
+		signals := strings.Split(signalPatterns, " ")
+
+		// split the output values into an array
+		output := strings.Split(outputValues, " ")
+
+		// add the signal and output to the map
+		allSignals[i] = signals
+		allOutput[i] = output
+	}
+
+	return allSignals, allOutput
+}
+
+// inferDigitsFromNumOfSignals takes a number of signals and returns the digit.
+func inferDigitsFromNumOfSignals(signal string) []int {
+	// create a map of the signals
+	var possibleDigits []int
+
+	// count the number of signals
+	numSignals := len(signal)
+
+	// loop through numOfSignalsToDigit
+	for digit, numOfSignals := range numOfSignalsToDigit {
+		// if the number of signals is the required number for the digit
+		if numSignals == numOfSignals {
+			// add the digit to the possible digits
+			possibleDigits = append(possibleDigits, digit)
+		}
+	}
+
+	// return the digit
+	return possibleDigits
+}
+
+// getPossibleOutputDigits returns the possible output digits.
+func getPossibleOutputDigits(output [][]string) map[int]map[string][]int {
+	// create a map of the possible digits
+	var possibleDigits = make(map[int]map[string][]int, len(output))
+
+	// loop through the output
+	for i, lines := range output {
+		// loop through the digits
+		for _, digit := range lines {
+			if _, ok := possibleDigits[i]; !ok {
+				// create the map
+				possibleDigits[i] = make(map[string][]int)
+			}
+
+			counter := 0
+
+			for {
+				newKey := fmt.Sprintf("%s_%d", digit, counter)
+
+				// if the digit is not in the map
+				if _, ok := possibleDigits[i][newKey]; !ok {
+					// add the digit to the map
+					possibleDigits[i][newKey] = inferDigitsFromNumOfSignals(digit)
+					break
+				}
+
+				counter += 1
+			}
+		}
+	}
+
+	// return the possible digits
+	return possibleDigits
+}
+
+// getSingleDigits returns the single digits from the possible output digits.
+func getSingleDigits(possibleOutputDigits map[int]map[string][]int) map[int]map[string][]int {
+	// create a map of the single digits
+	var singleDigits = make(map[int]map[string][]int)
+
+	// loop through the lines
+	for i, lineDigits := range possibleOutputDigits {
+		// loop through the possible digits
+		for signal, digits := range lineDigits {
+			// if there is only one digit
+			if len(digits) == 1 {
+				if _, ok := singleDigits[i]; !ok {
+					// create the map
+					singleDigits[i] = make(map[string][]int)
+				}
+
+				// if the digit is not in the map
+				if _, ok := singleDigits[i][signal]; !ok {
+					// add the digit to the map
+					singleDigits[i][signal] = digits
+				}
+			}
+		}
+	}
+
+	// return the single digits
+	return singleDigits
+}
+
+// countLenPerLine counts the number of unique signals per line.
+func countLenPerLine(output map[int]map[string][]int) int {
+	// create a map of the number of unique signals per line
+	numOfUniqueSignals := 0
+
+	// loop through the lines
+	for _, lineDigits := range output {
+		// loop through the digits
+		for _, digits := range lineDigits {
+			numOfUniqueSignals += len(digits)
+		}
+	}
+
+	// return the number of unique signals
+	return numOfUniqueSignals
+}
+
+// main is the entry point for the application.
+func main() {
+	// read the file
+	args := helpers.ReadArguments()
+	filename := args[0]
+	txtlines := helpers.ReadFile(filename)
+
+	// parse the file into signal patterns and output values
+	_, output := parseInput(txtlines)
+
+	// infer the digits from the number of signals for the output
+	possibleOutputDigits := getPossibleOutputDigits(output)
+
+	// print information
+	printOutputMap(possibleOutputDigits)
+	fmt.Println("Number of unique signals:", countLenPerLine(possibleOutputDigits))
+
+	// get the number of single digits for a given signal
+	numOfSingleDigits := getSingleDigits(possibleOutputDigits)
+
+	// print information
+	printOutputMap(numOfSingleDigits)
+	fmt.Println("Number of unique signals with single possible digits:", countLenPerLine(numOfSingleDigits))
+}
