b15e3b5dacfd5100f86b2b6da1e4e13e8dbc3172
João Carmo <kingjoao@gmail.com>
2022-12-05 01:05:17 +0000

    feat: Adds solution for day 4, part 2

--- 2022/04/main.go
+++ 2022/04/main.go
@@ -14,8 +14,26 @@ const SECTION_SEPARATOR = "-"
 // Section represents a section of the cleaning area [start, end]
 type Section []int
 
-// isOverlapping checks if the sections are overlapping
-func isOverlapping(sections []Section) bool {
+// isPartiallyOverlapping checks if the sections are overlapping at all
+func isPartiallyOverlapping(sections []Section) bool {
+	sectionA := sections[0]
+	sectionB := sections[1]
+
+	sectionAStart := sectionA[0]
+	sectionAEnd := sectionA[1]
+
+	sectionBStart := sectionB[0]
+	sectionBEnd := sectionB[1]
+
+	if sectionAEnd >= sectionBStart && sectionBEnd >= sectionAStart {
+		return true
+	}
+
+	return false
+}
+
+// isFullyOverlapping checks if the sections are fully overlapping
+func isFullyOverlapping(sections []Section) bool {
 	sectionA := sections[0]
 	sectionB := sections[1]
 
@@ -36,12 +54,25 @@ func isOverlapping(sections []Section) bool {
 	return false
 }
 
-// findOverlappingSections finds the overlapping sections
-func findOverlappingSections(cleaningSections [][]Section) [][]Section {
+// findPartiallyOverlappingSections finds the overlapping sections
+func findPartiallyOverlappingSections(cleaningSections [][]Section) [][]Section {
 	var overlappingSections [][]Section
 
 	for _, sections := range cleaningSections {
-		if isOverlapping(sections) {
+		if isPartiallyOverlapping(sections) {
+			overlappingSections = append(overlappingSections, sections)
+		}
+	}
+
+	return overlappingSections
+}
+
+// findFullyOverlappingSections finds the fully overlapping sections
+func findFullyOverlappingSections(cleaningSections [][]Section) [][]Section {
+	var overlappingSections [][]Section
+
+	for _, sections := range cleaningSections {
+		if isFullyOverlapping(sections) {
 			overlappingSections = append(overlappingSections, sections)
 		}
 	}
@@ -79,12 +110,22 @@ func main() {
 	filename := args[0]
 	txtlines := helpers.ReadFile(filename)
 
-	// part 1
+	// process the file
 	cleaningSections := getCleaningSectionsFromInput(txtlines)
-	overlappingSections := findOverlappingSections(cleaningSections)
-	overlappingPairs := len(overlappingSections)
+
+	// part 1
+	fullyOverlappingSections := findFullyOverlappingSections(cleaningSections)
+	fullyOverlappingPairs := len(fullyOverlappingSections)
 	fmt.Printf(
 		"[Part One] The answer is: %d\n",
+		fullyOverlappingPairs,
+	)
+
+	// part 2
+	overlappingSections := findPartiallyOverlappingSections(cleaningSections)
+	overlappingPairs := len(overlappingSections)
+	fmt.Printf(
+		"[Part Two] The answer is: %d\n",
 		overlappingPairs,
 	)
 }
